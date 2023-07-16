64809a8606d9f562733516c5fee497518968d2d4
personage-hub <76659786+personage-hub@users.noreply.github.com>
2023-07-16 15:22:48 +0300

    initial commit

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -19,6 +21,23 @@ func parseFlag() {
 	flag.IntVar(&reportIntervalParam, "r", 10, "Report interval for sending metrics to the server")
 	flag.IntVar(&pollIntervalParam, "p", 2, "Poll interval for collecting metrics")
 	flag.Parse()
+
+	if envValue := os.Getenv("ADDRESS"); envValue != "" {
+		serverAddress = envValue
+	}
+	if envValue := os.Getenv("REPORT_INTERVAL"); envValue != "" {
+		if intValue, err := strconv.Atoi(envValue); err == nil {
+			reportIntervalParam = intValue
+		}
+	}
+	if envValue := os.Getenv("POLL_INTERVAL"); envValue != "" {
+		if intValue, err := strconv.Atoi(envValue); err == nil {
+			pollIntervalParam = intValue
+		}
+	}
+
+	reportInterval = time.Duration(reportIntervalParam) * time.Second
+	pollInterval = time.Duration(pollIntervalParam) * time.Second
 	reportInterval = time.Duration(reportIntervalParam) * time.Second
 	pollInterval = time.Duration(pollIntervalParam) * time.Second
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 var serverAddress string
 
 func parseFlags() {
 	flag.StringVar(&serverAddress, "a", "localhost:8080", "Address of the HTTP server endpoint")
+
+	if envValue := os.Getenv("ADDRESS"); envValue != "" {
+		serverAddress = envValue
+	}
 
 	flag.Parse()
 }
