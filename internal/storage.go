4b43771e81ff276250fdebabd40b78ff795f71af
personage-hub <76659786+personage-hub@users.noreply.github.com>
2023-07-23 12:05:47 +0300

    Storage fix

--- internal/storage.go
+++ internal/storage.go
@@ -1,54 +1,64 @@
 package internal
 
+import (
+	"github.com/cornelk/hashmap"
+)
+
 type MemStorage struct {
-	gauge   map[string]float64
-	counter map[string]int64
+	gauge   *hashmap.Map[string, float64]
+	counter *hashmap.Map[string, int64]
 }
 
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
-		gauge:   make(map[string]float64),
-		counter: make(map[string]int64),
+		gauge:   hashmap.New[string, float64](),
+		counter: hashmap.New[string, int64](),
 	}
 }
 
 func (m *MemStorage) GaugeUpdate(key string, value float64) {
-	if m.gauge == nil {
-		m.gauge = make(map[string]float64)
-	}
-
-	if _, ok := m.gauge[key]; ok {
-		m.gauge[key] = value
-	} else {
-		m.gauge[key] = value
-	}
+	m.gauge.GetOrInsert(key, value)
 }
 
 func (m *MemStorage) CounterUpdate(key string, value int64) {
-	if m.counter == nil {
-		m.counter = make(map[string]int64)
-	}
-	if oldValue, ok := m.counter[key]; ok {
-		m.counter[key] = oldValue + value
+	current, ok := m.counter.Get(key)
+	if !ok {
+		m.counter.Set(key, value)
 	} else {
-		m.counter[key] = value
+		m.counter.Set(key, current+value)
 	}
 }
 
 func (m *MemStorage) GaugeMap() map[string]float64 {
-	return m.gauge
+	resultMap := make(map[string]float64)
+	m.gauge.Range(func(key string, value float64) bool {
+		resultMap[key] = value
+		return true
+	})
+	return resultMap
 }
 
 func (m *MemStorage) CounterMap() map[string]int64 {
-	return m.counter
+	resultMap := make(map[string]int64)
+	m.counter.Range(func(key string, value int64) bool {
+		resultMap[key] = value
+		return true
+	})
+	return resultMap
 }
 
 func (m *MemStorage) GetGaugeMetric(metricName string) (float64, bool) {
-	value, ok := m.gauge[metricName]
-	return value, ok
+	value, ok := m.gauge.Get(metricName)
+	if ok {
+		return value, true
+	}
+	return 0, false
 }
 
 func (m *MemStorage) GetCounterMetric(metricName string) (int64, bool) {
-	value, ok := m.counter[metricName]
-	return value, ok
+	value, ok := m.counter.Get(metricName)
+	if ok {
+		return value, true
+	}
+	return 0, false
 }
