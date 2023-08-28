301aebcc5a7faebe8b23e308d15fef8a1b2b96b0
personage-hub <76659786+personage-hub@users.noreply.github.com>
2023-08-28 22:57:29 +0300

    test fix

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -64,7 +64,7 @@ func parseFlags() Config {
 	if envValue := os.Getenv("STORE_INTERVAL"); envValue != "" {
 		val, err := strconv.ParseInt(envValue, 10, 64)
 		if err == nil {
-			fmt.Errorf("failed parse interval value: %w", err)
+			_ = fmt.Errorf("failed parse interval value: %w", err)
 		}
 		config.StoreInterval = val
 	}
@@ -76,7 +76,7 @@ func parseFlags() Config {
 	if envValue := os.Getenv("RESTORE"); envValue != "" {
 		boolValue, err := strconv.ParseBool(envValue)
 		if err != nil {
-			fmt.Errorf("failed parse restore flag: %w", err)
+			_ = fmt.Errorf("failed parse restore flag: %w", err)
 		}
 		config.Restore = boolValue
 	}
